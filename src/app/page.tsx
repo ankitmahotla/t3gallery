3ebd280bb6e5d4b37f0efac85c2d8d896a96e631
Ankit Mahotla <ankitmahotla45@gmail.com>
2024-04-17 18:22:41 +0530

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const mockUrls = [
   "https://utfs.io/f/39b20fe3-5d7f-4d5c-9d8e-7f2999a9fda1-cghdv3.jpg",
   "https://utfs.io/f/7b66b6bd-bc8c-4431-b61f-1aef68f9f948-6vm94g.jpg",
@@ -10,10 +12,15 @@ const mockImages = mockUrls.map((url, index) => ({
   url
 }))
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany()
+  console.log(posts)
   return (
     <main>
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {
           mockImages.map((image) => (
             <div key={image.id} className="w-48">
