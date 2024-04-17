f369db91501f79aa54c4a448577c0bf944065a52
Ankit Mahotla <ankitmahotla45@gmail.com>
2024-04-17 17:27:09 +0530

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
+const mockUrls = [
+  "https://utfs.io/f/39b20fe3-5d7f-4d5c-9d8e-7f2999a9fda1-cghdv3.jpg",
+  "https://utfs.io/f/7b66b6bd-bc8c-4431-b61f-1aef68f9f948-6vm94g.jpg",
+  "https://utfs.io/f/4efb50a0-6734-4133-9e10-5ebfd2629cd7-cia7t7.jpg",
+  "https://utfs.io/f/95cc3a7c-5d7d-4927-bfd2-01c25ab56250-uikwby.jpg"
+]
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url
+}))
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-
-      hello gallery in progress
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {
+          mockImages.map((image) => (
+            <div key={image.id} className="w-48">
+              <img src={image.url} />
+            </div>
+          ))
+        }
+      </div>
     </main>
   );
 }
