38c8a36c7c1b72878ed219cf133ad600a9ea3031
Ankit Mahotla <ankitmahotla45@gmail.com>
2024-04-19 17:22:26 +0530

    fixed dialog and made component for image view

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,12 +1,11 @@
-import { getImage } from "~/server/queries";
+import FullPageImageView from "~/app/components/full-image-page";
 
-export default async function PhotoModal({
+export default function PhotoPage({
     params: { id: photoId },
 }: {
     params: { id: string };
 }) {
     const idAsNumber = Number(photoId)
     if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id")
-    const image = await getImage(idAsNumber)
-    return <div><img src={image.url} className="w-96" /></div>
+    return <FullPageImageView id={idAsNumber} />
 }
