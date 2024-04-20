ccdcecd519c4206526b515d4a829a59c9b52292d
Ankit Mahotla <ankitmahotla45@gmail.com>
2024-04-20 18:39:21 +0530

    initial zustand setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,18 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
-import Image from "next/image";
-import Link from "next/link";
 import { getMyImages } from "~/server/queries";
+import { Images } from "./_components/images";
 
 export const dynamic = "force-dynamic"
 
-async function Images() {
-  const images = await getMyImages();
-
-  return (
-    <div className="flex flex-wrap items-end justify-center gap-4 p-4">
-      {images.map((image) => (
-        <div key={image.id} className="flex w-48 flex-col">
-          <Link href={`/img/${image.id}`}>
-            <Image
-              src={image.url}
-              style={{ objectFit: "contain" }}
-              width={192}
-              height={192}
-              className="max-h-48"
-              alt={image.name}
-            />
-          </Link>
-          <div className="text-center mt-2">{image.name.slice(0, 10)}</div>
-        </div>
-      ))}
-    </div>
-  );
-}
-
 export default async function HomePage() {
+  const images = await getMyImages();
   return (
     <main>
       <SignedOut>
         <div className="h-full w-full text-2xl text-center">Please sign in </div>
       </SignedOut>
       <SignedIn>
-        <Images />
+        <Images images={images} />
       </SignedIn>
     </main>
   );
